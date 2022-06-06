5921027c0b5071cd845eeea2ed7f0c4364e9fb22
m-ttaylor <mttaylor@email.wm.edu>
2022-06-06 11:45:10 -0400

    Add toggle details button to blog component

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,43 @@
-const Blog = ({blog}) => (
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
   <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+    <button 
+      style={hideWhenVisible} 
+      onClick={toggleVisibility}>
+      view
+    </button>
+    <div style={showWhenVisible}>
+      <div style={blogStyle}>
+        <div>
+          {blog.title} {blog.author}
+          <button onClick={toggleVisibility}>hide</button>
+        </div>
+        <div>likes: {blog.likes} <button>like</button></div>
+        <div>{blog.url}</div>
+        <div>{blog.user.name}</div>
+      </div>
+    </div>
+  </div>
+  )
+}
 
 export default Blog
