7a51ca12fa77a0632baffc897f77e17391fad279
m-ttaylor <mttaylor@email.wm.edu>
2022-06-13 15:38:26 -0400

    Track note app redux, clone unicafe-redux

--- part6/counter/src/App.js
+++ part6/counter/src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { legacy_createStore as createStore } from 'redux'
+
+const counterReducer = (state, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    case 'ZERO':
+      return 0
+    default:
+      return state
+  }
+}
+
+const store = createStore(counterReducer)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div>
+    <div>
+      {store.getState()}
     </div>
+    <button
+      onClick={e => store.dispatch({ type: 'INCREMENT' })}
+    >
+      plus
+    </button>
+    <botton
+      onClick={e => store.dispatch({ type: 'DECREMENT' })}
+    >
+      minus
+    </botton>
+    <botton
+      onClick={e => store.dispatch({ type: 'ZERO' })}
+    >
+      zero
+    </botton>
+  </div>
   );
 }
 
--- part6/counter/src/index.js
+++ part6/counter/src/index.js
@@ -1,15 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import logo from './logo.svg';
+import './App.css';
+import { legacy_createStore as createStore } from 'redux'
+
+const counterReducer = (state=0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    case 'ZERO':
+      return 0
+    default:
+      return state
+  }
+}
+
+const store = createStore(counterReducer)
+
+function App() {
+  return (
+  <div>
+    <div>
+      {store.getState()}
+    </div>
+    <button
+      onClick={e => store.dispatch({ type: 'INCREMENT' })}
+    >
+      plus
+    </button>
+    <button
+      onClick={e => store.dispatch({ type: 'DECREMENT' })}
+    >
+      minus
+    </button>
+    <button
+      onClick={e => store.dispatch({ type: 'ZERO' })}
+    >
+      zero
+    </button>
+  </div>
+  );
+}
+
+const renderApp = () => {
+  ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
