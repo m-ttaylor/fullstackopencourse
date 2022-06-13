c7c6bc62aa902c3149a4b9aeceb790485ed39028
m-ttaylor <mttaylor@email.wm.edu>
2022-06-13 17:19:07 -0400

    uncontrolled form and forwarding redux-store

--- part6/temp-redux-notes/src/index.js
+++ part6/temp-redux-notes/src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { legacy_createStore as createStore } from 'redux'
+import { Provider } from 'react-redux'
+import noteReducer from './reducers/noteReducer'
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const store = createStore(noteReducer)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>, document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
