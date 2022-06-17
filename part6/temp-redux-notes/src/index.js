fd4366c016348874fa6ed4755815f519b7c1a945
m-ttaylor <mttaylor@email.wm.edu>
2022-06-17 16:25:49 -0400

    Lessons

--- part6/temp-redux-notes/src/index.js
+++ part6/temp-redux-notes/src/index.js
@@ -2,33 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { legacy_createStore as createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-import { configureStore } from '@reduxjs/toolkit'
+import store from './store'
 
-import noteReducer from './reducers/noteReducer'
-import filterReducer from './reducers/filterReducer'
-
-import reportWebVitals from './reportWebVitals';
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer
-  }
-})
-
-console.log(store.getState())
-
-store.subscribe(() => console.log(store.getState()))
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
   </Provider>
 );
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
