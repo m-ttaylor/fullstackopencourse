0e0c50e33b5ebf66eeaa8207159c1cee4029dab7
m-ttaylor <mttaylor@email.wm.edu>
2022-09-09 11:47:59 -0400

    random prettier changes

--- part6/exercises/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/exercises/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,50 +1,52 @@
-import { createSlice } from '@reduxjs/toolkit'
-import anecdoteService from '../services/anecdotes'
+import { createSlice } from "@reduxjs/toolkit";
+import anecdoteService from "../services/anecdotes";
 
 const anecdoteSlice = createSlice({
-  name: 'anecdotes',
+  name: "anecdotes",
   initialState: [],
   reducers: {
     upvoteAnecdote(state, action) {
-      const id = action.payload
-      const anecdoteToChange = state.find(a => a.id === id)
+      const id = action.payload;
+      const anecdoteToChange = state.find((a) => a.id === id);
       const changedAnecdote = {
         ...anecdoteToChange,
-        votes: anecdoteToChange.votes+1
-      }
-      return state.map(a =>
-        a.id !== id ? a : changedAnecdote
-      )
+        votes: anecdoteToChange.votes + 1,
+      };
+      return state.map((a) => (a.id !== id ? a : changedAnecdote));
     },
     setAnecdotes(state, action) {
-      return action.payload
+      return action.payload;
     },
     appendAnecdote(state, action) {
-      state.push(action.payload)
-    }
-  }
-})
+      state.push(action.payload);
+    },
+  },
+});
 
 export const initializeAnecdotes = () => {
-  return async dispatch => {
-    const anecdotes = await anecdoteService.getAll()
-    dispatch(setAnecdotes(anecdotes))
-  }
-}
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch(setAnecdotes(anecdotes));
+  };
+};
 
-export const createAnecdote = content => {
-  return async dispatch => {
-    const newAnecdote = await anecdoteService.createNew(content)
-    dispatch(appendAnecdote(newAnecdote))
-  }
-}
+export const createAnecdote = (content) => {
+  return async (dispatch) => {
+    const newAnecdote = await anecdoteService.createNew(content);
+    dispatch(appendAnecdote(newAnecdote));
+  };
+};
 
 export const voteOnAnecdote = (id, updatedAnecdote) => {
-  return async dispatch => {
-    console.log('the anecdote object being sent to the server', updatedAnecdote)
-    await anecdoteService.updateVote(id, updatedAnecdote)
-    dispatch(upvoteAnecdote(id))
-  }
-}
-export const { appendAnecdote, upvoteAnecdote, setAnecdotes } = anecdoteSlice.actions
-export default anecdoteSlice.reducer
+  return async (dispatch) => {
+    console.log(
+      "the anecdote object being sent to the server",
+      updatedAnecdote
+    );
+    await anecdoteService.updateVote(id, updatedAnecdote);
+    dispatch(upvoteAnecdote(id));
+  };
+};
+export const { appendAnecdote, upvoteAnecdote, setAnecdotes } =
+  anecdoteSlice.actions;
+export default anecdoteSlice.reducer;
